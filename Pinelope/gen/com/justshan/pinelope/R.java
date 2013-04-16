88476420b218860650288b742b4333092206cb5c
Shanna Cote <shanna@sitesquared.com>
2013-04-16 16:53:59 -0400

    main page set up

--- Pinelope/gen/com/justshan/pinelope/R.java
+++ Pinelope/gen/com/justshan/pinelope/R.java
@@ -22,12 +22,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int pinelope=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int imageView1=0x7f080000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int userentry=0x7f030001;
     }
     public static final class menu {
         public static final int pinelope=0x7f070000;
@@ -35,7 +38,6 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
