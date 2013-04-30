95693a161c337db921e2ae980e16d020f6040120
Shanna Cote <shanna@sitesquared.com>
2013-04-30 15:01:05 -0400

    Added StaggeredGridView to repo

--- Pinelope/src/com/justshan/pinelope/PinsPage.java
+++ Pinelope/src/com/justshan/pinelope/PinsPage.java
@@ -14,6 +14,7 @@ import org.json.JSONObject;
 
 import com.origamilabs.library.views.StaggeredGridView;
 
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
